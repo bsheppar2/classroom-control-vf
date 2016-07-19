78c53a64ecb0396ad33fbafb11f98c25a29f802b
bsheppar2 <bsheppar_2000@yahoo.com>
2016-07-19 17:35:42 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,12 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  
+  file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Interesting class, still would like to know why this is called Puppet.\n",
+}
 }
